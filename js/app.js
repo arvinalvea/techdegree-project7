1e94dc05193694e018fb42e2b5f49879179595df
Arvin Alvea <arvinalvea@gmail.com>
2020-06-24 14:59:04 -0400

    Added 'Mobile Users' doughnut chart

--- js/app.js
+++ js/app.js
@@ -59,6 +59,35 @@ let dailyChart = new Chart(dailyCanvas, {
     options: dailyOptions
 });
 
+const mobileCanvas = document.getElementById("mobile-chart");
+const mobileData = {
+    labels: ["Desktop", "Tablet", "Phones"],
+    datasets: [{
+        label: '# of Users',
+        data: [2000, 550, 500],
+        borderWidth: 0,
+        backgroundColor: [
+            '#7477BF',
+            '#78CF82',
+            '#51B6C8'
+        ]
+    }]
+};
+const mobileOptions = {
+    legend: {
+        position: 'right',
+        labels: {
+            boxWidth: 20,
+            fontStyle: 'bold'
+        }
+    }
+}
+let mobileChart = new Chart(mobileCanvas, {
+    type: 'doughnut',
+    data: mobileData,
+    options: mobileOptions
+});
+
 
 // create the html for the banner
 alertBanner.innerHTML = 
